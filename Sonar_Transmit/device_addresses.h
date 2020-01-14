a31845942c4a052f905d4455070880f35928c780
Yuri-Njathi <yurinjathi@gmail.com>
2020-01-14 16:49:36 +0300

    Sonar tranmission before read.u16

---
+++ Sonar_Transmit/device_addresses.h
@@ -0,0 +1,23 @@
+/************************************************************************
+Copyright 2018-2019 Ciira wa Maina
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+**************************************************************************/
+#ifndef _DEVICE_ADDRESSES_H
+#define _DEVICE_ADDRESSES_H
+
+static uint32_t DEVADDR = 0x260115C8;
+static uint8_t NWKSKEY[] = { 0x60, 0x42, 0xF8, 0x05, 0xA3, 0x44, 0x61, 0x06, 0xF2, 0xDD, 0x00, 0x74, 0x72, 0xC6, 0x1B, 0x64 };
+static uint8_t APPSKEY[] =  { 0xAE, 0x83, 0xFF, 0xE6, 0x64, 0x32, 0x2C, 0xFA, 0x5C, 0x05, 0x84, 0x33, 0xE7, 0x9C, 0x52, 0xC0 };
+
+
+
+#endif
